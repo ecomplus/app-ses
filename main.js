fd856c96b8c281d7bdf3235c9c7e072d7f78e734
Leonardo Matos <leomp120894@gmail.com>
2019-06-13 18:50:43 -0300

    fix: checking ecomServerIps for non-GET requests

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const logger = require('console-files')
 const bodyParser = require('body-parser')
 const sqlite = require('sqlite3').verbose()
-const { ecomAuth } = require('ecomplus-app-sdk')
+const { ecomAuth, ecomServerIps } = require('ecomplus-app-sdk')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,7 +12,22 @@ const db = new sqlite.Database(process.env.ECOM_AUTH_DB)
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  if (req.method !== 'GET' && process.env.NODE_ENV === 'production') {
+    // check if request is comming from E-Com Plus servers
+    if (ecomServerIps.indexOf(req.get('x-real-ip')) === -1) {
+      res.status(403).send('Who are you? Unauthorized IP address')
+    } else {
+      next()
+    }
+  } else {
+    // bypass
+    next()
+  }
+})
 app.use(require('./lib/routes')({ ecomAuth, db }))
+
 app.listen(port)
 logger.log(`--> Starting web app on port :${port}`)
 
