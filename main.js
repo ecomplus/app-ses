b56ceb6582811d0a61f56c38cbadd766803ad64d
Leonardo Matos <leomp120894@gmail.com>
2019-06-13 18:03:44 -0300

    fix: stop listening uncaughtException, already done by console-files

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const logger = require('console-files')
 const bodyParser = require('body-parser')
 const sqlite = require('sqlite3').verbose()
 const { ecomAuth } = require('ecomplus-app-sdk')
@@ -7,32 +8,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-process.on('uncaughtException', (err) => {
-  // fatal error
-  // log to file before exit
-  let msg = '\n[' + new Date().toString() + ']\n'
-  if (err) {
-    if (err.hasOwnProperty('stack')) {
-      msg += err.stack
-    } else if (err.hasOwnProperty('message')) {
-      msg += err.message
-    } else {
-      msg += err.toString()
-    }
-    msg += '\n'
-  }
-  let fs = require('fs')
-  fs.appendFile('./_stderr', msg, () => {
-    process.exit(1)
-  })
-})
-
 const db = new sqlite.Database(process.env.ECOM_AUTH_DB)
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(require('./lib/routes')({ ecomAuth, db }))
 app.listen(port)
+logger.log(`--> Starting web app on port :${port}`)
 
 // daemon processes
 // prevent running background process on multiple servers
