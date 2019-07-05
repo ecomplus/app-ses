1d0b6960a0838e68f6cdb6f09ca23fb56893a1eb
Talisson Ferreira <talissonf@gmail.com>
2019-07-05 12:28:15 -0300

    test: test: handling bounces

--- main.js
+++ main.js
@@ -1,32 +1,18 @@
 'use strict'
 
 const logger = require('console-files')
-const bodyParser = require('body-parser')
 const sqlite = require('sqlite3').verbose()
-const { ecomAuth, ecomServerIps, setup } = require('ecomplus-app-sdk')
+const bodyParser = require('body-parser')
+const { ecomAuth, setup } = require('ecomplus-app-sdk')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
-// setup('/var/dbs/ses.sqlite')
+const port = process.env.PORT || 4222
+setup('/var/dbs/ses.sqlite')
 const db = new sqlite.Database(process.env.ECOM_AUTH_DB)
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.text())
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  if (req.method !== 'GET' && process.env.NODE_ENV === 'production') {
-    // check if request is comming from E-Com Plus servers
-    if (ecomServerIps.indexOf(req.get('x-real-ip')) === -1) {
-      res.status(403).send('Who are you? Unauthorized IP address')
-    } else {
-      next()
-    }
-  } else {
-    // bypass
-    next()
-  }
-})
 app.use(require('./lib/routes')({ ecomAuth, db }))
 
 app.listen(port)
